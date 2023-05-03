b5ad2c9016e8ff91277ac619b826efc3c48a4de8
svcbn <104844841+svcbn@users.noreply.github.com>
2023-05-04 03:37:35 +0900

    백준

---
+++ C# 백준/11328/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+int n = int.Parse(Console.ReadLine());
+StringBuilder t = new StringBuilder();
+char[] a = new char[26];
+char[] b = new char[26];
+
+for (int i = 0; i < n; i++)
+{
+    string[] s = Console.ReadLine().Split();
+
+    int r = 1;
+    Array.Clear(a);
+    Array.Clear(b);
+
+    foreach (var v in s[0])
+    {
+        a[v - 'a']++;
+    }
+    foreach (var v in s[1])
+    {
+        b[v - 'a']++;
+    }
+
+    for (int j = 0; j < 26; j++)
+    {
+        if (a[j] != b[j])
+        {
+            r = 0;
+            break;
+        }
+    }
+
+    t.Append(r == 1 ? "Possible\n" : "Impossible\n");
+
+}
+
+Console.Write(t);
--- C# 백준/13300/Program.cs
+++ C# 백준/13300/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] n = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] a = new int[12];
+
+for (int i = 0; i < n[0]; i++)
+{
+    int[] t = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    a[(t[1] - 1) * 2 + t[0]]++;
+}
+
+int r = 0;
+foreach (int i in a)
+{
+    r += i % n[1] == 0 ? i / n[1] : i / n[1] + 1;
+}
+Console.Write(r);
