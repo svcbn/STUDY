309d6d87363afe52fee9966356d7ccb55ea59878
svcbn <104844841+svcbn@users.noreply.github.com>
2023-05-11 02:41:11 +0900

    백준

---
+++ C# 백준/1158/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+List<int> l = new List<int>();
+List<int> m = new List<int>();
+
+for (int i = 1; i <= a[0]; i++) l.Add(i);
+
+int t = 0;
+for (int i = 0; i < a[0]; i++)
+{
+    t += a[1] - 1;
+    while (t >= l.Count) t -= l.Count;
+    m.Add(l[t]);
+    l.RemoveAt(t);
+}
+Console.Write("<" + string.Join(", ", m) + ">");
---
+++ C# 백준/1406/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+LinkedList<char> l = new();
+LinkedListNode<char> c = new('0');
+
+foreach (var v in Console.ReadLine().ToCharArray())
+{
+    l.AddLast(v);
+}
+l.AddLast(c);
+
+int n = int.Parse(Console.ReadLine());
+StringBuilder a = new();
+
+for (int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine();
+    switch (s[0])
+    {
+        case 'L':
+            if (c.Previous != null) c = c.Previous;
+            break;
+        case 'D':
+            if (c.Next != null) c = c.Next;
+            break;
+        case 'B':
+            if (c.Previous != null) l.Remove(c.Previous);
+            break;
+        case 'P':
+            l.AddBefore(c, s[2]);
+            break;
+    }
+}
+
+foreach (var v in l) a.Append(v);
+a.Remove(a.Length - 1, 1);
+Console.WriteLine(a);
