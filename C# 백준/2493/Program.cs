e0230ed69fcce1e381ae708266429e6420a042c2
svcbn <104844841+svcbn@users.noreply.github.com>
2023-05-30 09:02:55 +0900

    Feat: 백준

--- C# 백준/2493/Program.cs
+++ C# 백준/2493/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+int[] t = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+StringBuilder a = new();
+
+Stack<int[]> c = new Stack<int[]>();
+
+c.Push(new int[] { 100000001, 0 });
+
+for (int i = 1; i <= n; i++)
+{
+    while (c.Peek()[0] < t[i - 1])
+    {
+        c.Pop();
+    }
+
+    a.Append(c.Peek()[1] + " ");
+    c.Push(new int[] { t[i - 1], i });
+}
+
+Console.Write(a);
